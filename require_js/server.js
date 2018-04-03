71e8f888826dd1b883f9f657b8051678378d1262
Young Zhang <young.zhang@oocl.com>
2018-04-03 16:18:12 +0800

    Add Koa

--- require_js/server.js
+++ require_js/server.js
@@ -1,9 +1,44 @@
-const express = require('express');
-const app = express();
+const Koa = require('koa');
+const app = new Koa();
+const route = require('koa-route');
+const serve = require('koa-static');
 const path = require('path');
 const port = 3001;
 
-app.use(express.static(path.resolve('./public')));
+const about = ctx => {
+    ctx.response.type = 'html';
+    ctx.response.body = '<a href="/">Index Page</a>';
+};
+
+const logger = (ctx, next) => {
+    console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`);
+    next();
+};
+
+const handler = async (ctx, next) => {
+    try {
+        await next();
+    } catch (err) {
+        ctx.response.status = err.statusCode || err.status || 500;
+        ctx.response.type = 'html';
+        ctx.response.body = '<p>Something wrong, please contact administrator.</p>';
+        ctx.app.emit('error', err, ctx);
+    }
+};
+
+const main = ctx => {
+    ctx.throw(500);
+};
+
+app.use(handler);
+app.use(route.get('/five', main));
+app.use(route.get('/about', about));
+app.use(serve(path.resolve('./public')));
+app.use(logger);
+app.on('error', (err) => {
+   console.log(`logging error ${err.message}`);
+   console.log(err);
+});
 
 app.listen(port, (err) => {
     if (err) {
